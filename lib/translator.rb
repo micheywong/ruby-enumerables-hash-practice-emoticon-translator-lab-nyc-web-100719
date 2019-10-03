1339ed265c816b084e824ca6705584f7499935c1
Michelle Wong <michellekwong2@gmail.com>
2019-10-03 15:15:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,9 +24,9 @@ def get_japanese_emoticon(file_path, emoticon)
     return sorry_message
   end
   japanese_version
-  #binding.pry
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  hash = load_library(file_path)
+  english_version = hash[:get_meaning][emoticon]
 end
