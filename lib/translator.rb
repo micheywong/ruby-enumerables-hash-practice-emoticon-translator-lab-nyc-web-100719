4160f8fa0e0d6bb78665c1086560a90b9d2629ea
Michelle Wong <michellekwong2@gmail.com>
2019-10-03 14:20:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)
+  
   #returns japanese equivalent of english grinning
   #japanese_version = emoticon[key][1]
   binding.pry
